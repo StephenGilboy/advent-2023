673e0bb0167a3641121c928ef25ca09adbd63892
Stephen Gilboy <stephen.gilboy@agoda.com>
2023-12-04 11:10:54 -0600

    Day 4 first half

---
+++ src/day_04.rs
@@ -0,0 +1,67 @@
+pub struct Card {
+  id: i32,
+  card_numbers: Vec<i32>,
+  winning_numbers: Vec<i32>
+}
+
+impl Card {
+  pub fn from(id: i32, line: &str) -> Card {
+    let split: Vec<&str> = line.split("|").collect();
+    let mut card_numbers: Vec<i32> = Vec::new();
+    let mut winning_numbers: Vec<i32> = Vec::new();
+
+    split[1].split(" ").for_each(|t| {
+      match i32::from_str_radix(t.trim(), 10) {
+        Ok(i) => card_numbers.push(i),
+        Err(_) => ()
+      }
+    });
+    split[0].split(" ").for_each(|t| {
+      match i32::from_str_radix(t.trim(), 10) {
+        Ok(i) => winning_numbers.push(i),
+        Err(_) => ()
+      }
+    });
+    Card {
+      id,
+      card_numbers,
+      winning_numbers
+    }
+  }
+
+  pub fn points(&self) -> i32 {
+    let mut p: i32 = 0;
+    self.card_numbers.iter().for_each(|n| {
+      if self.winning_numbers.iter().any(|wn| {
+        *wn == *n
+      }) {
+        if p == 0 {
+          p = 1
+        } else {
+          p = p * 2
+        }
+      }
+    });
+    p
+  }
+}
+
+#[test]
+fn card_from_should_create_card() {
+  let want = Card {
+    id: 1,
+    card_numbers: Vec::from_iter([41,48,83,86,17]),
+    winning_numbers: Vec::from_iter([83, 86, 6, 31, 17, 9, 48, 53])
+  };
+  let got = Card::from(1, " 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
+  
+  assert_eq!(want.id, got.id);
+  assert_eq!(want.card_numbers.len(), got.card_numbers.len());
+  assert_eq!(want.winning_numbers.len(), got.winning_numbers.len());
+}
+
+#[test]
+fn card_points_should_give_correct_answer() {
+  let got = Card::from(1, " 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
+  assert_eq!(got.points(), 8);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use std::fs::read_to_string;
 use std::env;
 
 use day_02::Game;
+use day_04::Card;
 mod day_01;
 
 mod day_02;
+
+mod day_04;
 
 fn main() {
     // Day 01
@@ -14,6 +17,9 @@ fn main() {
     // Day 02
     let answer2 = day02();
     println!("Day 2: {}, {}", answer2.0, answer2.1);
+
+    let answer4 = day04();
+    println!("Day 4: {}, {}", answer4.0, answer4.1);
 }
 
 fn day01() -> i32 {
@@ -47,4 +53,19 @@ fn day02() -> (i32, i32) {
         answer_two_sum += game.power();
     }
     (answer_one_sum, answer_two_sum)
+}
+
+fn day04() -> (i32, i32) {
+    let mut cards: Vec<Card> = Vec::new();
+    let file = env::current_dir().unwrap().join("input/day_04_input.txt");
+    let mut i = 1;
+    let mut sum = 0;
+    for line in read_to_string(file).unwrap().lines() {
+        let input: Vec<&str> = line.split(":").collect();
+        let card = Card::from(i, input[1]);
+        sum += card.points();
+        cards.push(card);
+        i += 1;
+    }
+    (sum, 0)
 }
